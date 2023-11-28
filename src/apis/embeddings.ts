bf73bbd958b519ba85dec28ecf4fe4df19173b10
noble-varghese <noblekvarghese96@gmail.com>
2023-11-28 18:04:29 +0530

    feat: adding the embeddings api

---
+++ src/apis/embeddings.ts
@@ -0,0 +1,25 @@
+import { ModelParams } from "src/_types/portkeyConstructs";
+import { ApiResource } from "../apiResource";
+import { APIPromise, RequestOptions } from "../baseClient";
+import { PROMPT_API } from "../constants";
+
+
+export interface EmbeddingsBody extends ModelParams {
+    input: string;
+    model?: string;
+}
+
+export type Embeddings = Record<string, any>
+
+export class Prompt extends ApiResource {
+    create(
+        _body: EmbeddingsBody,
+        opts?: RequestOptions
+    ): APIPromise<Embeddings> {
+        const body = _body
+        const response = this.post<Embeddings>(PROMPT_API, { body, ...opts })
+        return response
+    }
+}
+
+
