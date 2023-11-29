e03504cfcde5e20e0f6043448810220a06e9b2a0
noble-varghese <noblekvarghese96@gmail.com>
2023-11-29 21:06:31 +0530

    feat: embedding APIs

--- examples/fallback.ts
+++ examples/fallback.ts
@@ -1,20 +1,20 @@
-import { Portkey } from "../src";
+// import { Portkey } from "../src";
 
-const portkey = new Portkey({
-    apiKey:"your-portkey-api-key",
-    mode: "fallback",
-    llms: [
-        { provider: "openai", virtual_key: "open-ai-key-1234", trace_id: "1234", metadata: { hello: "world" } },
-        { provider: "cohere", virtual_key: "cohere-api-key-1234", trace_id: "1234", metadata: { hello: "world" } },
-    ]
-});
+// const portkey = new Portkey({
+//     apiKey:"your-portkey-api-key",
+//     mode: "fallback",
+//     llms: [
+//         { provider: "openai", virtual_key: "open-ai-key-1234", trace_id: "1234", metadata: { hello: "world" } },
+//         { provider: "cohere", virtual_key: "cohere-api-key-1234", trace_id: "1234", metadata: { hello: "world" } },
+//     ]
+// });
 
-async function main() {
-    const chatCompletion = await portkey.chatCompletions.create({
-        messages: [{ role: 'user', content: 'Say this is a test' }],
-    });
+// async function main() {
+//     const chatCompletion = await portkey.chatCompletions.create({
+//         messages: [{ role: 'user', content: 'Say this is a test' }],
+//     });
 
-    console.log(chatCompletion.choices);
-};
+//     console.log(chatCompletion.choices);
+// };
 
-main();
+// main();
