2af2baaf3c7a20eb4064280c046d57c2b71cff78
noble-varghese <noblekvarghese96@gmail.com>
2023-12-05 19:38:31 +0530

    feat: added Llamaindex integration in portkey

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import * as Types from "./_types/portkeyConstructs";
 import * as apis from "./apis";
 import * as client from "./client";
 import * as consts from "./constants";
+import * as llama from "./llms/llamaIndex"
 
 export import Portkey = client.Portkey;
 export import LLMOptions = Types.LLMOptions;
 export import PORTKEY_GATEWAY_URL = consts.PORTKEY_GATEWAY_URL
 export import createHeaders = apis.createHeaders
+export import PortkeyLlama = llama.PortkeyLlama
 
 export default Portkey;
