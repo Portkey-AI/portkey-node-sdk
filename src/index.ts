29c6a116f6b2072e1c2de8d905fca0d6d958a1f5
Noble Varghese <noblekvarghese96@gmail.com>
2023-12-12 11:08:00 +0530

    feat: removing the langchain and llama integrations (#17)

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,10 @@ import * as Types from "./_types/portkeyConstructs";
 import * as apis from "./apis";
 import * as client from "./client";
 import * as consts from "./constants";
-import * as llama from "./llms/llamaIndex"
-import * as langchain from "./llms/langchain"
 
 export import Portkey = client.Portkey;
 export import LLMOptions = Types.LLMOptions;
 export import PORTKEY_GATEWAY_URL = consts.PORTKEY_GATEWAY_URL
 export import createHeaders = apis.createHeaders
-export import PortkeyLlama = llama.PortkeyLlama
-export import PortkeyLangchain = langchain.PortkeyLangchain
 
 export default Portkey;
