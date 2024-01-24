2aad816735d2586e9e63cc6258cd08da30ec134e
visargD <visargdesai@gmail.com>
2024-01-24 15:14:01 +0530

    chore: remove deprecated interfaces

--- src/_types/portkeyConstructs.ts
+++ src/_types/portkeyConstructs.ts
@@ -1,35 +1,13 @@
-
-
-
 export interface RetrySettings {
     attempts: number;
     on_status_codes: Array<number>
 }
-
-export interface Constructs {
-    provider?: string;
-    api_key?: string;
-    virtual_key?: string;
-    cache?: boolean;
-    cache_age?: number;
-    cache_status?: string;
-    cache_force_refresh?: boolean;
-    trace_id?: string;
-    metadata?: Record<string, any>;
-    weight?: number;
-    retry?: RetrySettings;
-    deployment_id?: string;
-    resource_name?: string;
-    api_version?: string;
-}
-
 
 export interface Function {
     name: string;
     description: string;
     parameters: string;
 }
-
 
 export interface ModelParams {
     model?: string;
@@ -54,20 +32,10 @@ export interface ModelParams {
     organization?: string;
 }
 
-export interface ConversationInput {
-    prompt?: string;
-    messages?: Array<Message>
-}
-
 export interface Message {
     role: string
     content: string
 }
-
-export interface LLMOptions extends Constructs, ConversationInput, ModelParams {
-    override_params?: Record<string, any>
-}
-
 
 export const ModelParamsList = [
 	"model", "suffix", "max_tokens", "temperature", "top_k", "top_p", "n", "stop_sequences", "timeout", "functions", "function_call", "logprobs", "echo", "stop", "presence_penalty", "frequency_penalty", "best_of", "logit_bias", "user", "organization"
