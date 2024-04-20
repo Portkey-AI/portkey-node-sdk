f89c01c9faa00dfc57f799bc12c282cdb28ecb12
csgulati09 <chandeepsinghgulati@gmail.com>
2024-04-20 15:03:56 +0530

    fix: false in headers logic for falsy check

--- src/apis/createHeaders.ts
+++ src/apis/createHeaders.ts
@@ -5,7 +5,6 @@ export const createHeaders = (config: Record<string, any>): Record<string, strin
 
 	for (let k in config) {
 		let v = config[k];
-
 		if (isEmpty(v)) continue;
 
 		// convert to snakecase
@@ -13,6 +12,12 @@ export const createHeaders = (config: Record<string, any>): Record<string, strin
 			headers[k.toLowerCase()] = v || ""
 			continue
 		}
+
+		// false logic (enter all the cases here, where we take a boolean input)
+		if (k.toLocaleLowerCase() === "debug" || k.toLocaleLowerCase() === "cacheforcerefresh") {
+			v = v.toString()
+		}
+
 		k = k.replace('ID', 'Id')
 			.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`)
 		if (!isEmpty(v) && typeof v == "object") {
