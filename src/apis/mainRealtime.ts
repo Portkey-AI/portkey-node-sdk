68b6c32a589afd74acce8ef23cfe1bac5da68273
csgulati09 <chandeepsinghgulati@gmail.com>
2025-10-10 16:14:55 +0530

    feat: calls support added

--- src/apis/mainRealtime.ts
+++ src/apis/mainRealtime.ts
@@ -2,14 +2,22 @@ import { ClientSecretCreateParams } from 'openai/resources/realtime/client-secre
 import { ApiResource } from '../apiResource';
 import { ApiClientInterface } from '../_types/generalTypes';
 import { finalResponse, initOpenAIClient, overrideConfig } from '../utils';
-import { RequestOptions } from '../baseClient';
+import { APIPromise, RequestOptions } from '../baseClient';
 import { createHeaders } from './createHeaders';
+import {
+  CallAcceptParams,
+  CallReferParams,
+  CallRejectParams,
+} from 'openai/resources/realtime/calls';
 
 export class MainRealtime extends ApiResource {
   clientSecrets: ClientSecrets;
+  calls: Calls;
+
   constructor(client: any) {
     super(client);
     this.clientSecrets = new ClientSecrets(client);
+    this.calls = new Calls(client);
   }
 }
 
@@ -36,5 +44,82 @@ export class ClientSecrets extends ApiResource {
       .create(body, opts)
       .withResponse();
     return finalResponse(result);
+  }
+}
+
+export class Calls extends ApiResource {
+  constructor(client: any) {
+    super(client);
+  }
+
+  accept(
+    callID: string,
+    body: CallAcceptParams,
+    params?: ApiClientInterface,
+    opts?: RequestOptions
+  ): APIPromise<void> {
+    if (params) {
+      const config = overrideConfig(this.client.config, params.config);
+      this.client.customHeaders = {
+        ...this.client.customHeaders,
+        ...createHeaders({ ...params, config }),
+      };
+    }
+    const OAIclient = initOpenAIClient(this.client);
+    const result = OAIclient.realtime.calls.accept(callID, body, opts);
+    return result as any;
+  }
+
+  hangup(
+    callID: string,
+    params?: ApiClientInterface,
+    opts?: RequestOptions
+  ): APIPromise<void> {
+    if (params) {
+      const config = overrideConfig(this.client.config, params.config);
+      this.client.customHeaders = {
+        ...this.client.customHeaders,
+        ...createHeaders({ ...params, config }),
+      };
+    }
+    const OAIclient = initOpenAIClient(this.client);
+    const result = OAIclient.realtime.calls.hangup(callID, opts);
+    return result as any;
+  }
+
+  refer(
+    callID: string,
+    body: CallReferParams,
+    params?: ApiClientInterface,
+    opts?: RequestOptions
+  ): APIPromise<void> {
+    if (params) {
+      const config = overrideConfig(this.client.config, params.config);
+      this.client.customHeaders = {
+        ...this.client.customHeaders,
+        ...createHeaders({ ...params, config }),
+      };
+    }
+    const OAIclient = initOpenAIClient(this.client);
+    const result = OAIclient.realtime.calls.refer(callID, body, opts);
+    return result as any;
+  }
+
+  reject(
+    callID: string,
+    body: CallRejectParams | null | undefined = {},
+    params?: ApiClientInterface,
+    opts?: RequestOptions
+  ): APIPromise<void> {
+    if (params) {
+      const config = overrideConfig(this.client.config, params.config);
+      this.client.customHeaders = {
+        ...this.client.customHeaders,
+        ...createHeaders({ ...params, config }),
+      };
+    }
+    const OAIclient = initOpenAIClient(this.client);
+    const result = OAIclient.realtime.calls.reject(callID, body, opts);
+    return result as any;
   }
 }
